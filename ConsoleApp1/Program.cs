46d9a269016c618bb685b20ebb8feb6b44e37bbc
CJ3016374 <CJ3016374@LAB01-PC12>
2023-06-30 12:04:10 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pessoa;
+            float altura;
+            Console.WriteLine("escreva o numero do codigo do empregado");
+            pessoa = int.Parse(Console.ReadLine());
+            Console.WriteLine("agora, digite a altura da pessoa");
+            altura = float.Parse(Console.ReadLine());
+            Console.WriteLine("a altura do empregado " +pessoa+ "é " +altura+"m");
+            Console.ReadKey();
+        }
+    }
+}
